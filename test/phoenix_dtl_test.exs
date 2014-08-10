db8241abad36d53cb10b9992c1f56bcd28791b2e
Andreas Gerstmayr <andihit@gmail.com>
2014-08-10 17:07:22 +0200

    more tests

--- test/phoenix_dtl_test.exs
+++ test/phoenix_dtl_test.exs
@@ -28,7 +28,7 @@ defmodule PhoenixDtlTest do
     html = View.render(MyApp.Templates, "hello.html",
       template_data
     )
-    assert html == "<html>\n  <head>\n    <title>Welcome</title>\n  </head>\n\n  <body>\n    Hello John!\nWelcome!\n\n  </body>\n</html>\n"
+    assert html == "<html>\n  <head>\n    <title>Welcome</title>\n  </head>\n\n  <body>\n    Hello John!\nWelcome!\n  </body>\n</html>"
   end
 
   test "render a dtl template without layout" do
@@ -37,18 +37,27 @@ defmodule PhoenixDtlTest do
       "welcome_message" => "Welcome!"
     }
     html = View.render(MyApp.Templates, "hello.html", template_data)
-    assert html == "Hello John!\nWelcome!\n"
+    assert html == "Hello John!\nWelcome!"
   end
 
-  test "render a dtl template with a list" do
+  test "render a dtl template with a list, partial template and a filter" do
     template_data = %{
       "users" => [
         %User{name: "John", points: 20},
         %User{name: "Andi", points: 10},
         %User{name: "Hannah", points: 15}
-      ]
+      ],
+      "message" => "hello"
     }
     html = View.render(MyApp.Templates, "list.html", template_data)
-    assert html == "\nJohn\n\nAndi\n\nHannah\n\n"
+    assert html == "\nJohn\n\nAndi\n\nHannah\n\nPartial template with a filter: HELLO"
+  end
+
+  test "render a dtl template with template inheritance" do
+    template_data = %{
+      "header" => "some header"
+    }
+    html = View.render(MyApp.Templates, "child.html", template_data)
+    assert html == "<title>default title</title>\n\n<h1>some header</h1>\n\n<article>\nsome article\n</article>\n"
   end
 end
