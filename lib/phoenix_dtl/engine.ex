ef31874de7e6c3ea04445af9d5ebaa709f81d900
Andreas Gerstmayr <andihit@gmail.com>
2014-08-10 19:04:50 +0200

    ensure compiled template is loaded

--- lib/phoenix_dtl/engine.ex
+++ lib/phoenix_dtl/engine.ex
@@ -10,18 +10,22 @@ defmodule PhoenixDtl.Engine do
 
   """
   def precompile(file_path, func_name) do
-    result = :erlydtl.compile_file(String.to_char_list(file_path), String.to_char_list(func_name), [{:out_dir, false}])
-    case result do
-      {:ok, module} -> module
-      {:ok, module, _warnings} -> module
+    result = :erlydtl.compile_file(String.to_char_list(file_path), String.to_char_list(func_name), [{:binary, true}, {:out_dir, false}])
+    binary = case result do
+      {:ok, module, binary} -> binary
+      {:ok, module, binary, _warnings} -> binary
       :error -> raise Phoenix.Template.UndefinedError
-      {:error, errors} -> raise Phoenix.Template.UndefinedError, message: errors
+      {:error, errors, _warnings} -> raise Phoenix.Template.UndefinedError, message: errors
     end
 
-    quote bind_quoted: [func_name: func_name] do
+    quote bind_quoted: [func_name: func_name, binary: binary] do
       def render(unquote(func_name), assigns) do
-        result = unquote(:"#{func_name}").render(PhoenixDtl.Util.convert_assignments(assigns))
+        case :code.is_loaded(unquote(:"#{func_name}")) do
+          false -> :code.load_binary(unquote(:"#{func_name}"), unquote(:"#{func_name}"), unquote(binary))
+          _ ->
+        end
 
+        result = unquote(:"#{func_name}").render(PhoenixDtl.Util.convert_assignments(assigns))
         case result do
           {:ok, rendered} -> IO.iodata_to_binary(rendered)
           {:error, errors} -> raise Phoenix.Template.UndefinedError, message: errors
