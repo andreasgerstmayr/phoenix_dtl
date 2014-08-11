7ff9914be0b4cb92cbe257f4d65f3f2298f82c4a
Andreas Gerstmayr <andihit@gmail.com>
2014-08-11 21:20:12 +0200

    use file_path instead of func_name for the module name to prevent clashing template file names (thanks to chrismccord for discovering this bug)

--- lib/phoenix_dtl/engine.ex
+++ lib/phoenix_dtl/engine.ex
@@ -10,7 +10,7 @@ defmodule PhoenixDtl.Engine do
 
   """
   def precompile(file_path, func_name) do
-    result = :erlydtl.compile_file(String.to_char_list(file_path), String.to_char_list(func_name), [{:binary, true}, {:out_dir, false}])
+    result = :erlydtl.compile_file(String.to_char_list(file_path), String.to_char_list(file_path), [{:binary, true}, {:out_dir, false}])
     binary = case result do
       {:ok, module, binary} -> binary
       {:ok, module, binary, _warnings} -> binary
@@ -18,14 +18,14 @@ defmodule PhoenixDtl.Engine do
       {:error, errors, _warnings} -> raise Phoenix.Template.UndefinedError, message: errors
     end
 
-    quote bind_quoted: [func_name: func_name, binary: binary] do
+    quote bind_quoted: [func_name: func_name, file_path: file_path, binary: binary] do
       def render(unquote(func_name), assigns) do
-        case :code.is_loaded(unquote(:"#{func_name}")) do
-          false -> :code.load_binary(unquote(:"#{func_name}"), unquote(:"#{func_name}"), unquote(binary))
+        case :code.is_loaded(unquote(:"#{file_path}")) do
+          false -> :code.load_binary(unquote(:"#{file_path}"), unquote(:"#{file_path}"), unquote(binary))
           _ ->
         end
 
-        result = unquote(:"#{func_name}").render(PhoenixDtl.Util.convert_assignments(assigns))
+        result = unquote(:"#{file_path}").render(PhoenixDtl.Util.convert_assignments(assigns))
         case result do
           {:ok, rendered} -> IO.iodata_to_binary(rendered)
           {:error, errors} -> raise Phoenix.Template.UndefinedError, message: errors
